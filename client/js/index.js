804050748d1427199261cc7d72c16d53e1a9852d
Margarita <Margarita>
2023-10-26 23:26:32 +0300

    feat: add form layout

--- client/js/index.js
+++ client/js/index.js
@@ -1,6 +1,8 @@
-import { data } from './data'
+import Inputmask from "inputmask";
 import 'scss/style.scss'
 
-console.log('hello')
+const phoneInput = document.querySelector(".input_phone");
+Inputmask("+375-99-999-99-99").mask(phoneInput);
 
-data()
+const button = document.querySelector(".form__button");
+button.addEventListener('click', () => document.querySelectorAll('.input').forEach(item => console.log(item.value)))
